b67df9b4ca782b9d0af43532e15a32639843c00b
Ralph <njugunaralph@gmail.com>
2025-06-18 17:10:13 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+console.log(countDown(10));
